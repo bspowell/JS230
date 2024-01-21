7c0d53711519296378fc2ada655f09e8464b1621
bspowell <82974206+bspowell@users.noreply.github.com>
2024-01-21 14:45:41 -0800

    Jan 21st

---
+++ review/exercises/event_async/randomizer.js
@@ -0,0 +1,8 @@
+
+document.querySelector('html').addEventListener('click', () => {
+  document.querySelector('#container').style = 'display: none';
+});
+
+document.querySelector('#container').addEventListener('click', event => {
+  event.stopPropagation();
+});
